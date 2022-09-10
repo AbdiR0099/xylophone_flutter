8d6a047c8b79033a30ce70bd10926848ad3ecfff
AbdiR0099 <arsaieed@gmail.com>
2022-09-10 13:55:53 +0500

    Xylophone with functions

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,58 @@ void main() {
 
 class Xylophone extends StatelessWidget {
   const Xylophone({Key? key}) : super(key: key);
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({Color color = Colors.red, int soundNumber = 0}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  // style: ButtonStyle(
-                  // fixedSize: MaterialStateProperty.all(Size(100, 100))),
-                  onPressed: () async {
-                    final player = AudioPlayer();
-                    player.setVolume(0.5);
-                    player.play(AssetSource('note1.wav'));
-                  },
-                  child: const Text('Click Me'),
-                ),
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.amber, soundNumber: 1),
+              buildKey(color: Colors.green, soundNumber: 2),
+              buildKey(color: Colors.blueAccent, soundNumber: 3),
+              buildKey(color: Colors.orange, soundNumber: 4),
+              buildKey(color: Colors.red, soundNumber: 5),
+              buildKey(color: Colors.cyan, soundNumber: 6),
+              buildKey(color: Colors.deepPurple, soundNumber: 7),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+// void main(){
+//   int step1 = add(n1: 5, n2: 9);
+//   int step2 = multiply (step1,5);
+//   double result = step2 / 3;
+//   print('$result');
+// }
+//
+// int add({int n1=1, int n2=1}){
+//   int addition = n1 + n2;
+//   return addition;
+// }
+//
+// int multiply(int a, int b){
+//   return a*b;
+// }
